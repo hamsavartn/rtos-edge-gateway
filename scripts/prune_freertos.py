2e2fa419452b73b17a9c7da59b6be8bfe7bb7a0e
Hamsavarthan_ <hamsavarthan2211@gmail.com>
2026-06-30 06:52:44 +0000

    Fix: keep only heap_4.c to avoid multiple pvPortMalloc definitions

--- scripts/prune_freertos.py
+++ scripts/prune_freertos.py
@@ -1,12 +1,14 @@
 import shutil, os
 base = ".pio/libdeps/bluepill_f103c8/FreeRTOS-Kernel/portable"
-keep = {"GCC", "MemMang", "Common"}
+keep_top = {"GCC", "MemMang", "Common"}
+
 if os.path.isdir(base):
     for item in os.listdir(base):
-        if item not in keep:
+        if item not in keep_top:
             path = os.path.join(base, item)
             if os.path.isdir(path):
                 shutil.rmtree(path)
+
     gcc_path = os.path.join(base, "GCC")
     if os.path.isdir(gcc_path):
         for item in os.listdir(gcc_path):
@@ -14,3 +16,9 @@ if os.path.isdir(base):
                 path = os.path.join(gcc_path, item)
                 if os.path.isdir(path):
                     shutil.rmtree(path)
+
+    heap_path = os.path.join(base, "MemMang")
+    if os.path.isdir(heap_path):
+        for item in os.listdir(heap_path):
+            if item != "heap_4.c":
+                os.remove(os.path.join(heap_path, item))
